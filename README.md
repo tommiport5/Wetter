a379cad8161966a22c8f723064454adaa00574e4
tommiport5 <tommi.port5.gmail.com>
2023-03-27 10:38:28 +0200

    Initial commit
    The basic version

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Wetter
  Das zweite Projekt für Software zum Anfassen
+## Was machen wir diesmal ?
+Diesmal arbeiten wir mit C++ unter Arduino. Wir "fritzen" uns einen ESP8266 Mikrokontrollermit einem Temperatur- und Feuchtigkeits-Sensor. 
+Zuerst werdendie Daten nur über die serielle Schnittstelle angezeigt, aber danach kommen auch Internet- und IOT-Konzepte zur Sprache.
---
+++ implementation/Wetter/Wetter.ino
@@ -0,0 +1,62 @@
+/***************************************************************************
+  This is a library for the SHT21 humidity, temperature & pressure sensor
+
+ Based on an example made by adafruit
+  ----> http://www.adafruit.com/products/2650
+
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit andopen-source hardware by purchasing products
+  from Adafruit!
+
+  Written by Limor Fried & Kevin Townsend for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+  See the LICENSE file for details.
+ ***************************************************************************/
+
+#include <Wire.h>
+#include <SHT21.h>
+
+const int sda_pin = 2;
+const int scl_pin = 4;
+
+SHT21 sht; // I2C
+
+unsigned long delayTime = 1000;
+
+void setup() {
+    Serial.begin(115200);
+    while(!Serial);    // time to get serial running
+    Serial.println(F("SHT21 test"));
+
+    unsigned status;
+    
+    // default settings
+    Wire.begin(sda_pin, scl_pin);
+
+    int SNo = sht.getSerialNumber(I2C_ADD);
+   
+    Serial.print("Serialnumber is: 0x"); Serial.println(SNo,16);
+    delayTime = 1000;
+
+    Serial.println();
+}
+
+
+void loop() { 
+    printValues();
+    delay(delayTime);
+}
+
+
+void printValues() {
+    Serial.print("Temperature = ");
+    Serial.print(sht.getTemperature());
+    Serial.println(" °C");
+
+    Serial.print("Humidity = ");
+    Serial.print(sht.getHumidity());
+    Serial.println(" %");
+
+    Serial.println();
+}
